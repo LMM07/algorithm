f69e0cb4dae930955e572eef1fd43e3496934f41
LMM07 <mm_0028@naver.com>
2022-03-23 23:34:13 +0900

    2022-03-23

--- src/level2/B2480.java
+++ src/level2/B2480.java
@@ -12,6 +12,34 @@ public class B2480 {
 		int y = scanner.nextInt();
 		int z = scanner.nextInt();
 		
+		if(x==y && y==z) {
+			System.out.println(10000+(x*1000));
+		} else if(x==y || x==z) {
+			System.out.println(1000+(x*100));
+		} else if(y==z) {
+			System.out.println(1000+(y*100));
+		} else {
+			System.out.println((Math.max(Math.max(x, y),z)*100));
+		}
+			
+		
+//		처음 작성한 코드. 답은 맞게 나오지만 너무 지저분하다.
+//		if(x==y) {
+//			if(x==z) {
+//				System.out.println(10000+(x*1000));
+//			} else {
+//				System.out.println(1000+(x*100));
+//			}
+//		} else if(x==z) {
+//			System.out.println(1000+(x*100));
+//		} else {
+//			int max = x;
+//			if(y>max) {
+//				max = y;
+//			} else if(z>max) {
+//				max = z;
+//			}
+//			System.out.println(max*100);
+//		}
 	}
-
 }
--- src/level2/B2525.java
+++ src/level2/B2525.java
@@ -12,6 +12,16 @@ public class B2525 {
 		int B = scanner.nextInt();
 		int C = scanner.nextInt();
 		
+		int H = A + ((B+C)/60);
+		int M = B + C;
+		
+		if(H >= 24) {
+			H = H-24;
+		}
+		if(M >= 60) {
+			M = (B+C)%60;
+		}
+		System.out.println(H + " " + M);
 	}
 
 }
